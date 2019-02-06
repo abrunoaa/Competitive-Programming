58cc9e1ab517d49806b09143d048b0b0509db00f
jockey6 <abrunoaa@gmail.com>
2019-02-06 13:01:39 -0200

    Summer School problems

---
+++ Solutions/ICPC Summer School 2019/Dia 1/Problem C. Cargo Transportation.cpp
@@ -0,0 +1,39 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+#define maxw (2 * 10003)
+
+typedef long long ll;
+
+int can[103][2 * maxw];
+
+int main(){
+  // freopen("in","r",stdin);
+  cin.sync_with_stdio(0), cin.tie(0);
+
+  int n, l, w, r;
+  cin >> n >> l >> w >> r;
+  w *= 2;
+  r *= 2;
+  can[0][0 + maxw] = 1;
+  can[0][0 + maxw + 1] = -1;
+  for(int i = 1; i <= n; ++i){
+    int m;
+    cin >> m;
+    for(int x = -w; x <= w; ++x){
+      can[i - 1][x + maxw] += can[i - 1][x - 1 + maxw];
+      if(can[i - 1][x + maxw]){
+        can[i][max((ll)-w, x - (ll)l * m) + maxw] += 1;
+        can[i][max((ll)-w, x - (ll)r * m + 1) + maxw] -= 1;
+        can[i][min((ll)w + 1, x + (ll)r * m) + maxw] += 1;
+        can[i][min((ll)w + 1, x + (ll)l * m + 1) + maxw] -= 1;
+      }
+    }
+  }
+  bool ans = 0;
+  for(int x = -w; x <= w; ++x){
+    ans |= can[n][x + maxw] += can[n][x - 1 + maxw];
+  }
+  cout << (ans ? "Yes" : "No") << '\n';
+  return 0;
+}
---
+++ Solutions/ICPC Summer School 2019/Dia 1/Problem D. Divisibility - Iterative.cpp
@@ -0,0 +1,63 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+#define st first
+#define nd second
+#define inf 0x3f3f3f3f
+
+typedef pair<int,int> ii;
+
+#define maxn 1000003
+
+vector<ii> pd[maxn][2];
+
+int p;
+string l;
+
+int main(){
+  // freopen("in","r",stdin);
+  cin.sync_with_stdio(0), cin.tie(0);
+
+  cin >> l >> p;
+  for(int z = 0; z < 2; ++z){
+    pd[l.size()][z].resize(p);
+    for(int r = 0; r < p; ++r){
+      pd[l.size()][z][r] = ii(-inf * (r != 0 || !z), -1);
+    }
+  }
+  for(int i = l.size() - 1; i >= 0; --i){
+    for(int z = 0; z < 2; ++z){
+      pd[i][z].resize(p);
+      for(int r = 0; r < p; ++r){
+        if(!z && l[i] == '0'){
+          pd[i][z][r] = max(ii(1, 0), pd[i + 1][0][0]);
+        }
+        else{
+          ii t = pd[i + 1][1][(r * 10 + l[i] - '0') % p];
+          ++t.st;
+          t.nd = l[i] - '0';
+          pd[i][z][r] = max(t, pd[i + 1][z][r]);
+        }
+      }
+    }
+  }
+  if(pd[0][0][0].st <= 0){
+    cout << "-1\n";
+    return 0;
+  }
+  for(int i = 0, r = 0, z = 0; i < (int)l.size() && pd[i][z][r].st; ++i){
+    if(!z && l[i] == '0'){
+      if(pd[i][0][0] != pd[i + 1][0][0]){
+        cout << '0';
+        break;
+      }
+    }
+    else if(pd[i][z][r] != pd[i + 1][z][r] || l[i] - '0' == pd[i + 1][z][r].nd){
+      cout << l[i];
+      r = (r * 10 + l[i] - '0') % p;
+      z = 1;
+    }
+  }
+  cout << '\n';
+  return 0;
+}
