957de0c187b8ddd7a26a11e4c2ccf60e33e3a071
jockey6 <abrunoaa@gmail.com>
2019-04-16 17:09:03 -0300

    Update fast IO

--- Codes/Misc/Fast IO.cc
+++ Codes/Misc/Fast IO.cc
@@ -6,16 +6,16 @@ struct IO{
 
   inline operator bool(){ return cur; }
 
-  inline void readc(){ cur = getc(stdin); }
-  inline bool isblank(){ return cur <= ' '; }
-  inline bool isdigit(){ return (unsigned char)(cur - '0') <= 9; }
-  inline void skipBlanks(){ while(isblank()) readc(); }
+  inline static bool isblank(char c){ return c <= ' '; }
+  inline static bool isdigit(char c){ return c <= '9' && c >= '0'; }
+  inline char readc(){ return cur = (char)getc(stdin); }
+  inline void skipBlanks(){ while(isblank(cur)) readc(); }
 
   inline IO& operator >> (char &c){ skipBlanks(); c = cur; readc(); return *this; }
 
   inline IO& operator >> (char *s){
     skipBlanks();
-    while(*s++ = cur, readc(), !isblank());
+    while(*s++ = cur, !isblank(readc()));
     *s = 0;
     return *this;
   }
@@ -23,21 +23,19 @@ struct IO{
   inline IO& operator >> (string &s){
     s.clear();
     skipBlanks();
-    while(s.push_back(cur), readc(), !isblank());
+    while(s.push_back(cur), !isblank(readc()));
     return *this;
   }
 
-  inline IO& operator >> (double &d){ if(*this >> buf) sscanf(buf, "%lf", &d); return *this; }
-  inline IO& operator >> (long double &d){ if(*this >> buf) sscanf(buf, "%Lf", &d); return *this; }
+  inline IO& operator >> (double &d){ *this >> buf; sscanf(buf, "%lf", &d); return *this; }
+  inline IO& operator >> (long double &d){ *this >> buf; sscanf(buf, "%Lf", &d); return *this; }
 
-  template<class intType>
-  inline IO& operator >> (intType &n){
+  template<class intType> inline IO& operator >> (intType &n){
     skipBlanks();
     int sign = +1;
     if(cur == '-') sign = -1, readc();
-    else if(cur == '+') readc();
     n = cur - '0';
-    while(readc(), isdigit()) n += n + (n << 3) + cur - '0';
+    while(isdigit(readc())) n = (n << 1) + (n << 3) + cur - '0';
     n *= sign;
     return *this;
   }
@@ -51,21 +49,23 @@ struct IO{
   inline char* toString(double d){ sprintf(buf, "%.9lf%c", d, '\0'); return buf; }
   inline char* toString(long double d){ sprintf(buf, "%.9Lf%c", d, '\0'); return buf; }
 
-  template<class intType>
-  char* toString(intType n){
-    char* p = buf + 99;
+  template<class intType> inline char* toString(intType n){
+    char* p = buf + sizeof(buf) - 1;
     *p = 0;
     if(!n) *--p = '0';
     else{
       bool neg;
       if((neg = n < 0)) n = -n;
-      while(n) *--p = n % 10 + '0', n /= 10;
+      while(n) *--p = (char)(n % 10 + '0'), n /= 10;
       if(neg) *--p = '-';
     }
     return p;
   }
 
   template<class T> inline IO& operator << (T x){ return *this << toString(x); }
+
+  inline void sync_with_stdio(bool){}
+  inline void tie(void*){}
 };
 
 static IO __io__;
