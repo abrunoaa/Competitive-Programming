a1e4bbeee9cc3ce620f701fca4913ae635672408
jockey6 <abrunoaa@gmail.com>
2019-05-03 07:30:07 -0300

    New codes

--- Solutions/Codeforces/255C - Almost Arithmetical Progression.cpp
+++ Solutions/Codeforces/255C - Almost Arithmetical Progression.cpp
@@ -1,55 +1,77 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()  // 11.639s >> FU***!!!
-{
-  register int n, i, j, q, maxq, aux;
-  int N, b[4005], r, rep[1000005] = {}, *paux;
-  map < int, int > dp[4005];
+#ifdef BALLOONFIELD
+__attribute__((destructor))static void __destroy__(){cerr<<"\nElapsed: "<<(double)clock()/CLOCKS_PER_SEC<<"s.\n";}
 
-  scanf("%d", &N);
-  //N = 4000;
-  n = N;
+void debug(){}
+template<class t,class... u> void debug(const t&x,const u&...y){cerr<<' '<<x,debug(y...);}
+# define debug(x...) cerr<<"\x1b[91m"<<__LINE__<<" ["#x"] =",debug(x),cerr<<"\x1b[0m\n"
+# define sleep(x) this_thread::sleep_for(chrono::milliseconds(x))
+#else
+# define assert(x) void(0)
+# define debug(...) void(0)
+# define freopen(...) void(0)
+# define sleep(x) void(0)
+#endif
 
-  if (n <= 2)                               // so pode ter uma sequencia de n valores
-    maxq = n;
-  else
-  {
-    maxq = 2;                                     // minimo possivel / precisa?
-    for (i = 0; i < n; ++i)
-    {
-      scanf("%d", b + i);
-      //b[i] = i;
-      ++rep[b[i]];                                // qtd de vezes q bi aparece
-    }
-/*
-for (i = 0; i < n; ++i)
-  printf("%d ", b[i]);
-puts("\n");
-for (i = 0; i <= 1e6; ++i)
-  if (rep[i])
-    printf(" -> %d rep %d\n", i, rep[i]);
-puts("");
-//*/
-    for (i = 0; ++i < n;)                        // loop normal / pos 0 desnecessaria
-      for (j = i; --j >= 0; )                     // volta contando
-        if (rep[b[i]] > 1 || rep[b[j]] > 1)       // checa se pode ter sequencia???
-        {
-          r = b[i] - b[j];                        // ** i > j **
-          q = ((aux = dp[j][r]) ? aux + 1 : 2);   // se ja foi calculado: aproveita
-                                                  // >> TEM i E j NA SEQUENCIA == 2
-//printf(" ## %d %d == %d %d\n", i, j, r, q);
-          r *= -1;                // a proxima sequencia inverte a razao: 1 <- 2 <- 1
-          if (q > *(paux = &dp[i][r]))        // se é maior q a sequencia atual em dp
-          {
-//printf("   >> %d %d : %d %d\n", i, r, q, maxq);
-            if ((*paux = q) > maxq)               // atualiza / se é o maior de todos
-              maxq = q;
-          }
-        }
+template<class A,class B>ostream&operator<<(ostream&f,const pair<A,B>&x){return f<<'('<<x.first<<", "<<x.second<<')';}
+
+#define DEF1(_class...) ostream&operator<<(ostream&f,const _class&x){int b=0;f<<'[';for(auto&y:x){if(b++)f<<", ";f<<y;}return f<<']';}
+#define DEF2(get,pop,_class...) ostream&operator<<(ostream&f,_class x){int b=0;f<<'[';while(x.size()){if(b++)f<<", ";f<<x.get();x.pop();}return f<<']';}
+template<class T> DEF1(vector<T>) template<class T,class C> DEF1(set<T,C>) template<class T,class C> DEF1(multiset<T,C>) template<class K,class T,class C> DEF1(map<K,T,C>) template<class K,class T,class C> DEF1(multimap<K,T,C>)
+template<class T> DEF2(front,pop,queue<T>) template<class T> DEF2(top,pop,stack<T>) template<class T,class V,class C> DEF2(top,pop,priority_queue<T,V,C>)
+
+#define null nullptr
+#define st first
+#define nd second
+#define inf 0x3f3f3f3f
+#define infl 0x3f3f3f3f3f3f3f3f
+#define mod 1000000007
+#define maxn 4003
+
+#define rand() uid(rng)
+mt19937 rng(chrono::high_resolution_clock::now().time_since_epoch().count()); // ll = mt19937_64
+uniform_int_distribution<int> uid(0, numeric_limits<int>::max());             // range for random
+
+typedef long long ll;
+typedef double lf;
+typedef pair<int,int> ii;
+
+int x[maxn];
+int memo[maxn][maxn];
+
+int main(){
+  freopen("in","r",stdin);
+  cin.sync_with_stdio(0), cin.tie(0);
+
+  int n;
+  cin >> n;
+
+  map<int,int> mp;
+  for(int i = 0; i < n; ++i){
+    cin >> x[i];
+    mp[x[i]];
   }
 
-  printf("%d\n", maxq);
+  int k = 0;
+  for(auto &x : mp) x.nd = k++;
+  for(int i = 0; i < n; ++i) x[i] = mp[x[i]];
+
+  for(int i = 0; i < n; ++i)
+    for(int j = 0; j < n; ++j)
+      memo[i][j] = 1;
+
+  int ans = 1;
+  for(int i = n - 2; i >= 0; --i){
+    for(int j = i + 1; j < n; ++j){
+      int &pd = memo[i][x[j]];
+      pd = max(pd, 1 + memo[j][x[i]]);
+      ans = max(ans, pd);
+    }
+  }
+
+  cout << ans << '\n';
 
   return 0;
 }
