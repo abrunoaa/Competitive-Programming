27b09d1d4ca642100151f948459d002723a3952c
jockey6 <abrunoaa@gmail.com>
2020-02-24 11:03:40 -0300

    Add builder to factorial

--- Codes/Math/Combination.cpp
+++ Codes/Math/Combination.cpp
@@ -1,5 +1,13 @@
 int fat[maxn], finv[maxn];
 
+void build() {
+  fat[0] = finv[0] = 1;
+  for (int i = 1; i < maxn; ++i) {
+    fat[i] = (int)((ll)i * fat[i - 1] % mod);
+    finv[i] = mpow(fat[i], mod - 2, mod);
+  }
+}
+
 int nCr(int n, int r) {       // combinação
   return (ll)fat[n] * finv[r] % mod * finv[n - r] % mod;
   return fat[n] / (fat[r] * fat[n - r]);  // se n! não estourar
