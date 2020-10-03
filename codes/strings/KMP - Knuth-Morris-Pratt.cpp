969f522d31ae81be763cee45c96919b817e442d0
Bruno Almêda de Oliveira <abrunoaa@gmail.com>
2020-10-03 16:16:13 -0300

    fix code

--- codes/strings/KMP - Knuth-Morris-Pratt.cpp
+++ codes/strings/KMP - Knuth-Morris-Pratt.cpp
@@ -3,14 +3,14 @@ int n, m, b[maxn];
 
 void kmp() {        // pré processa o padrão
   b[0] = -1;
-  for (int i = 0, j = -1; i < n; b[++i] = ++j)
+  for (int i = 0, j = -1; i < m; b[++i] = ++j)
     while (j >= 0 && p[i] != p[j])
       j = b[j];
 }
 
 void match() {
   int i = 0, j = 0;
-  while (i <= n - m) {
+  while (i < n) {
     while (j >= 0 && s[i] != p[j])
       j = b[j];
     ++i;
