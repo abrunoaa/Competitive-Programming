38a91d23d0e77c1fe43223935f2be22def196e60
Bruno Almêda de Oliveira <abrunoaa@gmail.com>
2020-11-26 11:10:21 -0300

    add contest

--- Solutions/Contest/Asia-Pacific Informatics Olympiad, 2014/palindrome/run.sh
+++ Solutions/Contest/Asia-Pacific Informatics Olympiad, 2014/palindrome/run.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-for filename in [0-9][0-9]; do
-  echo $filename
-  ./"Palindromic Refrain" <$filename >result
-  ./check $filename "$filename.a" result
+g++ -O2 -o exe "$1" || exit 1
+g++ -O2 -o check "check.cpp" || exit 1
+
+for filename in [0-9][0-9].in; do
+  filename_no_ext=${filename::-3}
+  printf "$filename_no_ext: "
+  ./exe <$filename >result || exit 1
+  ./check $filename_no_ext "$filename_no_ext.out" result || exit 1
 done
 
 rm result
+rm exe
+rm check
