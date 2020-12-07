932e4a23ba16e931d09f9c8a8a0c03b446fa7069
Bruno Almêda de Oliveira <abrunoaa@gmail.com>
2020-12-07 07:38:09 -0300

    update files for book

--- codes/ds/PalindromicTree.cpp
+++ codes/ds/PalindromicTree.cpp
@@ -1,33 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int maxn = (int)1e5 + 3;
-
-struct PalindromicTree {  // ERRTREE
-  int root, empty, maxSuf, d;
+/** begin *********************************************************************/
+struct PalindromicTree {  // also known as ERRTREE
+  int root, empty, maxSuf, d; // d is the number of palindromes
   string str;
-  int length[maxn + 2];
-  int link[maxn + 2];
-  int occ[maxn + 2];          // frequency of each palindrome
-  map<char, int> child[maxn + 2];
+  vector<int> length, link, occ; // occ is the frequency of each palindrome
+  vector<map<char, int>> child;
 
-  PalindromicTree()
-      : root(maxn - 1), empty(0), maxSuf(root), d(0), str("$") {
-    length[0] = 0;
+  PalindromicTree(int n) : root(n), empty(0), maxSuf(root), d(0), str("$"),
+      length(n + 1), link(n + 1), occ(n + 1), child(n + 1) {
     length[root] = -1;
     link[0] = link[root] = root;
-    occ[0] = occ[root] = 0;
   }
 
-  bool add(char c) {
-    int n = (int)str.size() - 1;
-    auto up = [n, c, this](int p) {
-      for (; p != root && c != str[n - length[p]]; p = link[p]);
-      return p;
-    };
-
+  bool add(char c) {  // returns true if c adds a new palindrome substring
+    assert(str.size() < link.size());
     str.push_back(c);
-    maxSuf = up(maxSuf);
+    maxSuf = up(maxSuf, c);
     if (child[maxSuf].count(c)) {
       maxSuf = child[maxSuf][c];
       ++occ[maxSuf];
@@ -36,21 +26,31 @@ struct PalindromicTree {  // ERRTREE
 
     ++d;
     length[d] = length[maxSuf] + 2;
-    link[d] = maxSuf == root ? empty : child[up(link[maxSuf])][c];
+    link[d] = maxSuf == root ? empty : child[up(link[maxSuf], c)][c];
     occ[d] = 1;
     maxSuf = child[maxSuf][c] = d;
     return true;
   }
 
-  void updateAllOccurences() {
-    for (int v = d; v > 0; --v) occ[link[v]] += occ[v];
+  void updateAllOccurences() { for (int v = d; v > 0; --v) occ[link[v]] += occ[v]; }
+
+private:
+
+  int up(int p, char c) {
+    int k = (int)str.size() - 2;
+    for (; p != root && c != str[k - length[p]]; p = link[p]);
+    return p;
   }
-} p;
+};
+/** end ***********************************************************************/
 
 int main() {
-  string s = "aabcaa";
+  string s = "aabbaa";
+  PalindromicTree p((int)s.size());
   for (char c : s) {
-    cerr << " >> " << c << ' ' << p.add(c) << endl;
+    bool a = p.add(c);
+    cerr << " >> " << c << ' ' << a << endl;
+    assert(a);
   }
   cout << p.d << '\n';
   return 0;
